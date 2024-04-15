e2ecc3f58a0eb61fcfd288757367130835143d80
Melih Safa Çelik <45492251+MelihCelik00@users.noreply.github.com>
2024-04-15 12:33:59 +0300

    Time: 31 ms (91.86%) | Memory: 16.7 MB (16.62%) - LeetSync

--- 2436-make-array-zero-by-subtracting-equal-amounts/make-array-zero-by-subtracting-equal-amounts.py
+++ 2436-make-array-zero-by-subtracting-equal-amounts/make-array-zero-by-subtracting-equal-amounts.py
@@ -1,6 +1,6 @@
 class Solution:
     def minimumOperations(self, nums: List[int]) -> int:
-        non_unique_elems = {}
+        non_unique_elems = set()
 
         if [0]*len(nums) == nums:
             return 0
@@ -10,10 +10,10 @@ class Solution:
         
         for i in range(len(nums)):
             if nums[i] != 0:
-                if nums[i] in non_unique_elems.keys():
+                if nums[i] in non_unique_elems:
                     pass
                 else:
-                    non_unique_elems[nums[i]] = i
+                    non_unique_elems.add(nums[i])
 
         return len(non_unique_elems)
                 
