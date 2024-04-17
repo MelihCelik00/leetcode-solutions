f28a82aae7fa7c962b5b5f504b7ad511b0313955
Melih Safa Çelik <45492251+MelihCelik00@users.noreply.github.com>
2024-04-17 23:09:35 +0300

    Time: 36 ms (80.11%) | Memory: 17.5 MB (84.49%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.py
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.py
@@ -12,19 +12,33 @@ class Solution:
         
         # return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right)) # worst case O(n)
 
-        # Breadth First Search (Queue)
-        if not root:
-            return 0
+        # Breadth First Search (Queue Implementation)
+        # if not root:
+        #     return 0
 
-        level = 0
-        q = deque([root])
-        while q:
-            for i in range(len(q)):
-                node=q.popleft()
-                if node.left:
-                    q.append(node.left)
-                if node.right:
-                    q.append(node.right)
-            level += 1
+        # level = 0
+        # q = deque([root])
+        # while q:
+        #     for i in range(len(q)):
+        #         node=q.popleft()
+        #         if node.left:
+        #             q.append(node.left)
+        #         if node.right:
+        #             q.append(node.right)
+        #     level += 1
 
-        return level
+        # return level
+
+        # Iterative Depth First Search (Stack Implementation)
+        stack = [[root, 1]]
+        res = 0
+
+        while stack:
+            node, depth = stack.pop()
+
+            if node:
+                res = max(res, depth)
+                stack.append([node.right, depth+1])
+                stack.append([node.left, depth+1])
+        
+        return res
