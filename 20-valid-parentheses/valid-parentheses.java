3ad90480a29a702625799d9ef816a2c6be243c90
Melih Safa Çelik <45492251+MelihCelik00@users.noreply.github.com>
2024-12-24 22:30:30 +0300

    Time: 4 ms (15.48%) | Memory: 42.7 MB (7.59%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<String> stack = new Stack<String>();
+
+        HashMap<String, String> hashmap = new HashMap<>( Map.of(        
+            ")", "(",
+            "}", "{",
+            "]", "["
+        ));
+
+        for (int p = 0; p < s.length(); p++) {
+            String currentChar = Character.toString(s.charAt(p));
+
+            if (hashmap.containsValue(currentChar)) {
+                stack.push(currentChar);
+            }
+            else if (!stack.isEmpty() && hashmap.get(currentChar).equals(stack.peek())) {
+                stack.pop();
+            }
+            else {
+                return false;
+            }
+        }
+
+        return stack.isEmpty();
+    }
+
+    }
